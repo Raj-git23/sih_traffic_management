1ac10db7ff684ec83258cc9bb2fa8a71b8e36ff9
Raj-git23 <rajpaswan0875.com>
2024-09-06 22:20:16 +0530

    till presentation work

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,14 +1,14 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
-        center: { lat: -34.397, lng: 150.644 },
+        center: { lat: 28.7500, lng: 77.1175 },
         mapId: 'DEMO_MAP_ID', // Replace with your map ID if needed
         styles: [] // Ensure styles are set to an empty array or default
     });
 
     const marker = new google.maps.marker.AdvancedMarkerElement({
         map: map,
-        position: { lat: -34.397, lng: 150.644 },
+        position: { lat: 28.7500, lng: 77.1175 },
         title: "Hello World!",
     });
 
